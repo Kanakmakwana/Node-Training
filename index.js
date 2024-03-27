b3bbf880e0781f99a22d110d3143a7350f23d170
Kanakmakwana <111720071+Kanakmakwana@users.noreply.github.com>
2024-03-28 00:10:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,13 @@ connect();
 app.get('/api',(req,res)=>{
     res.send(data);
 })
-// const newData = req.body;
-// data.push(newData);
-// res.send(data);
-// console.log(data);
 
-// app.post("/",(req,res)=>{
-//     const newData =req.body;
-//     data.push(newData);
-//     res.send(data);
-//     console.log(data)
-// })
+app.post("/",(req,res)=>{
+    const newData =req.body;
+    data.push(newData);
+    res.send(data);
+    console.log(data)
+})
 app.listen(3000,()=>{
     console.log("server running at port 3000");
 })
